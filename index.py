d56e63035580e42dd3e2831783f6968fa4923380
rdale240 <33500259+rdale240@users.noreply.github.com>
2019-02-06 15:00:01 -0500

    Add files via upload
    Created Flow for Automation and Connected to mySQL Database

--- index.py
+++ index.py
@@ -1,47 +1,130 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from dotenv import load_dotenv
+import os
+import mysql.connector
+import time
 
-#Sample Data
+
+##REMAINING WORK:
+    #Get/Set last IDs to ENV from DB
+    #Add Program from DB to Program Drop Down
+    #Select Submit Button / Send Click()
+
+
+#Implement .env to protect database information
+load_dotenv()
+
+#location of chromedriver.exe
 chromedriverLocation = 'C:\\Users\\Richard\\chromedriver.exe'
-firstName='Hello World'
-firstName=''.join(e for e in firstName if e.isalnum())
-lastName='Dale%$#@'
-lastName=''.join(e for e in lastName if e.isalnum())
-email="someone@miami.edu"
-phone='5555555555' #no plus but can have parentheses
-country = "Malawi" #Must have first letter uppercase
-################################
 
+#load database information
+HOST=os.getenv("HOST")
+USER=os.getenv("USER")
+PASS=os.getenv("PASS")
+DB=os.getenv("DB")
+PORT=os.getenv("PORT")
+print(HOST,USER,PASS,PORT)
+
+#respective databases
+databases=["acctax","analytics","business","finance","gemba","hemba","intlbusiness","leadership","mba","mha","promba","sustainable"]
+
+#connect to mySQL server to acquire data to post to salesforce
+cnx = mysql.connector.connect(user=USER, password=PASS,
+                              host=HOST,
+                              database='umiami')
+
+print(cnx)
 #Open Chrome
 driver = webdriver.Chrome(chromedriverLocation)
-driver.get('http://go.miami.edu/specialized-masters') #URL must be custom for source
-##########################
 
-#First Name Input
-fNameInput=driver.find_element_by_name('First_Name__c')
-fNameInput.send_keys(firstName)
+#Automating Flow
+#Enter Every Database
+for database in databases:
+    #switch to choose corresponding salesforce URL
+    if (database=="leadership") or (database=="finance") or (database=="form") or (database=="intlbusiness") or (database=="analytics") or (database=="mha"):
+        url="http://go.miami.edu/specialized-masters"
+    elif (database=="gemba"):
+        url="http://go.miami.edu/ex-mba-americas"
+    elif (database=="hemba"):
+        url="http://go.miami.edu/ex-mba-health-sector"
+    elif (database=="promba"):
+        url="http://go.miami.edu/professional-mba"
+    elif (database=="business") or (database=="mba"):
+        url="http://go.miami.edu/full-time-mba"
+    else:
+        url="http://go.miami.edu/specialized-masters"
+    #Open mySQL Cursor
+    cursor=cnx.cursor()    
+    #SQL Query
+    query=("SELECT id, first_name, last_name, email, phone, utm_source, utm_medium, utm_campaign, program FROM "+database + " where ID > 600" ) # where ID > some ID related to DB
+    #Execute SQL Query
+    cursor.execute(query)
+    #For all data returned from Query, execute Automation
+    for(id,first_name,last_name,email,phone,utm_source,utm_medium,utm_campaign, program) in cursor:
+        print(id,first_name,last_name,email,phone,utm_source,utm_medium,utm_campaign, program)
+        #Go to Salesforce URL
+        if(utm_source==None):
+            utm_source=""
+        if(utm_medium==None):
+            utm_medium=""
+        if(utm_campaign==None):
+            utm_campaign=""
+        driver.get(url+'/?utm_source='+utm_source+'&utm_medium='+utm_medium+'&utm_campaign='+utm_campaign) #URL must be custom for source
+        ##########################
 
-#Last Name Input
-lNameInput=driver.find_element_by_name('Last_Name__c')
-lNameInput.send_keys(lastName)
-#####################################
+        #First Name Input
+        fNameInput=driver.find_element_by_name('First_Name__c')
+        first_name=''.join(e for e in first_name if e.isalnum())
+        fNameInput.send_keys(first_name)
 
-#Email Input
-emailInput=driver.find_element_by_name('Email__c')
-emailInput.send_keys(email)
-#####################################
+        #Last Name Input
+        lNameInput=driver.find_element_by_name('Last_Name__c')
+        last_name=''.join(e for e in last_name if e.isalnum())
+        lNameInput.send_keys(last_name)
+        #####################################
 
-#Phone Input
-phoneInput=driver.find_element_by_name('Home_Phone__c')
-phoneInput.send_keys(phone)
-######################################
+        #Email Input
+        emailInput=driver.find_element_by_name('Email__c')
+        emailInput.send_keys(email)
+        #####################################
+
+        #Phone Input
+        phoneInput=driver.find_element_by_name('Home_Phone__c')
+        phone=str(phone).replace("+","")
+        phoneInput.send_keys(phone)
+        ######################################
+        time.sleep(3)
+    #Close Cursor
+    cursor.close()
+#Close mySQL Connection after cycling through all DBs
+cnx.close()
+#################################################
+
+
+
+# #Sample Data
+# firstName='Hello World'
+# firstName=''.join(e for e in firstName if e.isalnum())
+# lastName='Dale%$#@'
+# lastName=''.join(e for e in lastName if e.isalnum())
+# email="someone@miami.edu"
+# phone='5555555555' #no plus but can have parentheses
+# country = "Malawi" #Must have first letter uppercase
+# ################################
+
+
+
+
+
 
 #Country Input (Not important)
 #Useful for Program Dropdown
-countryInput=driver.find_element_by_name("Mailing_Country__c")
-countryInputSelector = Select(countryInput)
-countryInputSelector.select_by_value(country)
+# countryInput=driver.find_element_by_name("Mailing_Country__c")
+# countryInputSelector = Select(countryInput)
+# countryInputSelector.select_by_value(country)
 ######################################
 
+#driver.get('http://go.miami.edu/specialized-masters')
 
 
